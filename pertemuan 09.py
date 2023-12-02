552818592cb84490e6303a81b41fe8ed33eb59be
Indah T Elisabeth <indahtriende06@gmail.com>
2023-12-02 14:46:02 +0700

    Menambahkan plotly-indah.py

--- pertemuan 09.py
+++ pertemuan 09.py
@@ -1,18 +1,30 @@
 import streamlit as st
 
+# Ini bagian heading aplikasi Streamlit
 st.title("Kuliah Praktikum Big Data")
 st.write("Indah")
 st.write("# Heading 1")
 st.write("## Heading 2")
 st.write("### Heading 3")
+st.write('#### Heading 4')
+st.write('##### Heading 6')
 
+# Kinerja Unit
+st.metric("Kinerja", 40,-1)
+st.metric("Response Time", 30, 20)
+
+# Pilihan
 pilih1 = st.checkbox('Ya')
 pilih2 = st.checkbox('Tidak')
 
 st.write(pilih1)
 st.write(pilih2)
 
-st.radio('Makanan kesukaan',['Bakso','Nasi goreng', 'Mie ayam'])
+"""
+Ini komentar harusnya
+"""
+
+makanan = st.radio('Makanan kesukaan',['Bakso','Nasi goreng', 'Mie ayam'])
 
 minuman = st.selectbox('Pilih minuman yang akan dipesan',
                        ['Es teh', 'Kopi', 'Jus'])
